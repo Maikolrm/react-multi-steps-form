9e228ac936356a3b1d31fae07b515af561b8da28
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-04 03:07:01 -0400

    Components - StepsSelector - adding component legends

--- src/components/StepsSelector.jsx
+++ src/components/StepsSelector.jsx
@@ -8,14 +8,20 @@ import Container from "./Container"
 
 export default function StepsSelector(props) {
   // app state
-  const { currentStep } = useContext(AppState)
+  const { steps, currentStep } = useContext(AppState)
 
   return(
-    <Container styles={props.styles}>
-      {props.steps.map((step, index) => (
-        <button key={index} onClick={() => alert("Select form step")} className={`block w-10 h-10 border border-wihte rounded-full font-bold text-xs text-white leading-10 ${currentStep == index ? "bg-sky-300 border-sky-300 text-purple-500" : ""}`}>
-          {index + 1}
-        </button>
+    <Container styles="absolute top-10 w-full flex gap-2 justify-center lg:flex-col lg:justify-none lg:gap-6">
+      {steps.map((step, index) => (
+        <Container styles="flex items-center lg:ml-12">
+          <button key={index} onClick={() => alert("Select form step")} className={`block w-10 h-10 border border-wihte rounded-full font-bold text-xs text-white leading-10 ${currentStep == index ? "bg-sky-300 border-sky-300 text-purple-500" : ""}`}>
+            {index + 1}
+          </button>
+          <Container styles="hidden pl-4 uppercase lg:block">
+            <p className="text-xs text-gray-300 leading-none">step {index + 1}</p>
+            <h3 className="mt-2 font-semibold text-sm text-white leading-none">{step.name}</h3>
+          </Container>
+        </Container>
       ))}
     </Container>
   )
