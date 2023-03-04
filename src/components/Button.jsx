37c22a0895e1063aed9bb50ac99378867c6eb5e2
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-03 21:39:04 -0400

    Components - Button - dispatching event ( set-current-step )

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,29 @@
+import { useContext } from "react"
+
+// context
+import AppState from "../context/AppState"
+import AppDispatch from "../context/AppDispatch"
+
 export default function Button(props) {
+  // app state
+  const { currentStep } = useContext(AppState)
+
+  // app dispatch
+  const appDispatch = useContext(AppDispatch)
+
+  // handleClick
+  function handleClick(action) {
+    if (action == "next") {
+      appDispatch({ type: "set-current-step", value: currentStep + 1 })
+    } else {
+      appDispatch({ type: "set-current-step", value: currentStep - 1 })
+    }
+  }
+
   return(
-    <button className="block w-24 h-12 bg-blue-800 rounded-md text-white capitalize">
+    <button
+      onClick={() => handleClick(props.action)}
+      className={`block w-28 h-12 rounded-md text-sm font-semibold capitalize ${props.styles}`}>
       {props.name}
     </button>
   )
