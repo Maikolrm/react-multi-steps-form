3179daf2e9f4b104ce70c1ec9af74e4cb50d5506
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-03 20:47:48 -0400

    Project - App - creating app basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
 
+// components
+import Container from "./components/Container"
+import Button from "./components/Button"
+import StepsSelector from "./components/StepsSelector"
+
 function App() {
   return (
-    <div className="m-auto">
-      <h1 className="text-red-500">welcome</h1>
-    </div>
+    <Container styles="flex flex-col w-full h-full m-auto bg-white rounded-xl shadow-md overflow-hidden">
+      <Container styles="relative overflow-hidden">
+        <img src="/sidebar-mobile.svg" alt="sidebar background" className="block w-full" />
+        <StepsSelector steps={[1, 2, 3, 4]} styles="absolute left-0 top-6 z-1 w-full flex justify-center gap-2" />
+      </Container>
+      <Container styles="flex-1">main content</Container>
+      <Container styles="p-3">
+        <Button name="next step" />
+      </Container>
+    </Container>
   )
 }
 
