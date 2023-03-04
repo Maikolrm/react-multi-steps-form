52168a883fa136eef7a11bed39860a90deb18bb1
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-03 21:39:36 -0400

    Components - StepsSelector - pulling currentStep from AppState

--- src/components/StepsSelector.jsx
+++ src/components/StepsSelector.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react"
+
+// context
+import AppState from "../context/AppState"
+
 // components
 import Container from "./Container"
 
 export default function StepsSelector(props) {
+  // app state
+  const { currentStep } = useContext(AppState)
+
   return(
     <Container styles={props.styles}>
       {props.steps.map((step, index) => (
-        <button key={index} className={`block w-10 h-10 border border-wihte rounded-full font-bold text-xs text-white leading-10`}>
+        <button key={index} onClick={() => alert("Select form step")} className={`block w-10 h-10 border border-wihte rounded-full font-bold text-xs text-white leading-10 ${currentStep == index ? "bg-sky-300 border-sky-300 text-purple-500" : ""}`}>
           {index + 1}
         </button>
       ))}
