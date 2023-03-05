cd39861169381372986c6c1c2cbb46eb932f2cf4
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-05 14:09:56 -0400

    Components - steps-form/FormHeader - adding design colors

--- src/components/steps-form/FormHeader.jsx
+++ src/components/steps-form/FormHeader.jsx
@@ -4,10 +4,10 @@ import Container from "../Container"
 export default function FormHeader(props) {
   return(
     <Container styles="pb-4">
-      <h2 className="font-bold text-2xl text-sky-700 capitalize leading-none">
+      <h2 className="font-bold text-2xl text-marine-blue capitalize leading-none">
         {props.heading}
       </h2>
-      <p className="mt-2 text-sm text-gray-500">
+      <p className="mt-2 text-base text-cool-gray first-letter:uppercase">
         {props.description}
       </p>
     </Container>
