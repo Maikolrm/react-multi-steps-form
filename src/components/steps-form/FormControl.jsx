9831388307a13765054930fc70aff259a4d4365d
Hernandez Maikol <maikol_hernandez_rm@hotmail.com>
2023-03-05 15:30:15 -0400

    Components - steps-form/FormControl - adding theme colors

--- src/components/steps-form/FormControl.jsx
+++ src/components/steps-form/FormControl.jsx
@@ -4,12 +4,12 @@ import Container from "../Container"
 export default function FormControl(props) {
   return(
     <Container styles="">
-      <label htmlFor={props.name} className="flex w-full pb-2 text-sm capitalize leading-none">
+      <label htmlFor={props.name} className="flex w-full pb-2 text-sm text-marine-blue capitalize leading-none">
         {props.label} <span className={`ml-auto font-semibold text-red-500 hidden`}>this field is required</span>
       </label>
       <input
         id={props.name}
-        className="outline-none block w-full px-4 bg-transparent border border-gray-300 rounded-md leading-[3rem]"
+        className="outline-none block w-full px-4 bg-transparent border border-gray-300 rounded-md text-cool-gray leading-[3rem]"
         type={props.type}
         name={props.name}
         value={props.value}
